47f3b183902e9357aa3d3922c7a79af4d273e3b7
Elias <eg378556@gmail.com>
2023-05-07 23:33:47 -0300

    Ejercicio 9 semana 1

---
+++ Ejercicio_9.py
@@ -0,0 +1,12 @@
+Num1 = int(input("Ingrese el primer numero \n"))
+Num2 = int(input("Ingrese el segundo numero \n"))
+
+Suma = Num1 + Num2 
+Resta = Num1 - Num2
+Multiplicacion = Num1 * Num2
+Division = Num1 // Num2
+
+print(f"""La suma de sus números da como resultado {Suma}
+La resta de sus números da como resultado {Resta}
+La multiplicación de sus números da como resultado {Multiplicacion}
+La división de sus números da como resultado {Division}""")
